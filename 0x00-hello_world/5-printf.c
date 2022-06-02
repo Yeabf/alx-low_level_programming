c60b59e9bd0433acb8de8e3531243ec1edf7eff4
Yeabf <yeabsiraf2@gmail.com>
2022-06-02 13:56:18 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main- used printf to output
+ *Return: is 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}

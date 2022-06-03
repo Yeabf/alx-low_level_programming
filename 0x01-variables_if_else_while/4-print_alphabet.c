c582a76bdd8fcf3680c4802eedd8db80c9a30b28
Yeabf <yeabsiraf2@gmail.com>
2022-06-03 11:22:26 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main- print letters except q and e
+ *Return: is 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+	
+	while (alpha <= 'z', !'q', !'e');
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	return (0);
+}
+

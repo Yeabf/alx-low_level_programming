cde4831baff12d47e23f91ad29030af2b3241365
Yeabf <yeabsiraf2@gmail.com>
2022-06-03 10:58:15 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main- output character in lowercase
+ *Return: is 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}

9ed09007abd9bf58575d527875c6874fa7abb484
Yeabf <yeabsiraf2@gmail.com>
2022-06-07 13:59:59 -0700

    c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,14 @@
 #include "main.h"
-/**
- * print_sign - validate the sing of a number
- * Description: validate if a argument(@n) is negative, postive o zero
- * @n: The number to checked
- * Return: 1,-1 or 0 according to the sign of the argument @n
- */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar('+' + 0);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar('-' + 0);
-		return (-1);
-	}
-	else
-	{
-		_putchar('0' + 0);
-		return (0);
-	}
-}
 
+/**
+ * _abs - return abs value
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}

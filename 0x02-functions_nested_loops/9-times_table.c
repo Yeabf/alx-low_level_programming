7a350622660c7a6d42692db52dea7a3aaa36b10e
Yeabf <yeabsiraf2@gmail.com>
2022-06-07 14:04:47 -0700

    c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,56 +1,46 @@
 #include "main.h"
+
 /**
- * times_table - perform the multiplication of the first 9 digits
- * Description: entry
- * Return: Void
+ * times_table - prints times table
+ * Return:void
  */
 
 void times_table(void)
 {
-	int i;
-	int j;
 
-	for (i = 0 ; i <= 9; ++i)
+int a = 0;
+int b;
+int rep;
+while (a <= 9)
+{
+	b = 0;
+	while (b <= 9)
 	{
-		for (j = 0 ; j <= 9; ++j)
+		rep = a * b;
+		if (b == 0)
 		{
-			print_no_zero(i * j, j);
+			_putchar('0' + rep);
 		}
-		_putchar('\n');
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
 	}
+	_putchar('\n');
+	a++;
 }
 
-/**
- * print_no_zero - No print tens  when is equal to '0'
- * @n: Number to cheked
- * @i: Number to indicate to multi
- * Return: Void
- */
-
-void print_no_zero(int n, int i)
-{
-	int units, tens;
-
-	tens = n / 10;
-	units = n % 10;
-
-	if (tens == 0)
-	{
-		if (i > 0)
-		{
-			_putchar(32);
-			_putchar(32);
-		}
-		_putchar(units + '0');
-		if (i < 9)
-			_putchar(44);
-	} else
-	{
-		if (i > 0)
-			_putchar(32);
-		_putchar(tens + '0');
-		_putchar(units + '0');
-		if (i < 9)
-			_putchar(44);
-	}
 }

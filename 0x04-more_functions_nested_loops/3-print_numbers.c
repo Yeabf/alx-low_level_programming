fb817e5a00325e369b1eb66a95940ce8a6c666ae
Yeabf <yeabsiraf2@gmail.com>
2022-06-09 13:27:09 -0700

    loop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- print from 0-9
+ * return: void
+ */
+void print_numbers(void)
+{
+	char n=0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+
+
+	}
+	_putchar('\n');
+}	
+

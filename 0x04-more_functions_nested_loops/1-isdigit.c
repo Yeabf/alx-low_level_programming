c1181dde4ad819f67725ff636c397d686b909441
Yeabf <yeabsiraf2@gmail.com>
2022-06-09 13:17:04 -0700

    loop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isupper - check for upper case letter
- * @c : character to check the case
+ * _isdigit - check for a digit
+ * @c : character to check
  * Return:0 or 1
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	return (c >= 65 && c <= 90);
+	return (c >= 48 && c <= 57);
 
 }

bc93b5aa969d405b246b6e861c5b05593c909c2f
Yeabf <yeabsiraf2@gmail.com>
2022-06-13 12:13:49 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strlen- lenght of a string
+ *@s: a character
+ *Return: is void
+ */
+
+int_strlen(char *s);
+{
+strlen(char *s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,5 @@
 #include <stdlib.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+#include <strlen.h>

58bade829e29557e110be2f36bd70437f9be899f
Yeabf <yeabsiraf2@gmail.com>
2022-06-16 11:13:15 -0700

    c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,28 @@
 #include "main.h"
+
 /**
- * _strcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
-*/
+ * _strcmp - Compare two strings
+ *
+ * @s1: First string
+ *
+ * @s2: Second string
+ *
+ * Return: 0 if equal, less than 1 or greater than 1
+ */
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
+	int diff = 0;
+	int i = 0;
 
-/**
-* We have reached the end of S1 OR we have reached the end of S2
-*
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-{
-	if (s1[i] != s2[i])
+	while (s1[i])
 	{
-		return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			diff = ((s1[i] - '\0') - (s2[i] - '\0'));
+			break;
+		}
+		i++;
 	}
-}
-return (0);
+	return (diff);
 }

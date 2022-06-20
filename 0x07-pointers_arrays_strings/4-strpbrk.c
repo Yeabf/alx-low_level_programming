874ef55e64978d26c211513f6cf535185a10a694
Yeabf <yeabsiraf2@gmail.com>
2022-06-20 10:06:18 -0700

    c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strbrk - search strings for any set of bytes
+ * @s: strings to be matched
+ * @accept: bytes containing the matched
+ * Return: s
+ */
+
+char *_strbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}

8e70b02d9b12369882841cb25e8d16b7b3707627
Yeabf <yeabsiraf2@gmail.com>
2022-06-20 06:50:05 -0700

    ponters

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memcpy- copies memory area
+ * @dest: destination memory area
+ * @src; origin area
+ * @n: amount of bytes written
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+unsigned int i;
+for (i = 0 ; n > 0 ; i++, n--)
+{
+dest[i] = src[i];
+}
+return (dest)
+}
+

5b6b4c798760c4a8d5ef47255b721c25f052d5a9
Yeabf <yeabsiraf2@gmail.com>
2022-06-25 12:04:46 -0700

    lib

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * @s: string s
+ * @accept: character to match
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, cmpt = 0;
+for (i = 0 ; s[i] >= '\0' ; i++)
+{
+for (j = 0; accept[j] > '\0' ; j++)
+{
+	if (s[i] == accept[j])
+	{
+	cmpt++;
+	break;
+	}
+}
+	if (accept[j] == '\0')
+{
+	break;
+}
+}
+	return (cmpt);
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strbrk - search strings for any set of bytes
+ * @s: strings to be matched
+ * @accept: bytes containing the matched
+ * Return: pointer p is not null
+ */
+
+char *_strbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}

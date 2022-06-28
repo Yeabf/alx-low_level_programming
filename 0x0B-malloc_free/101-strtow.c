637dac351a8c0b9f22d065ee766df0df237d1c53
Yeabf <yeabsiraf2@gmail.com>
2022-06-28 16:32:00 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,19 +9,20 @@
 int _strlen(char *s)
 {
 	int size = 0;
+
 	for (; s[size] != '\0'; size++)
 		;
 	return (size);
 }
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
+ * *str_addChar - concatenates two strings
+ * @str: string 1
+ * @c: string 2
  * Return: pointer
  */
 
-char *str_addChar (char *str, char c)
+char *str_addChar(char *str, char c)
 {
 	int size, i;
 	char *m;
@@ -51,7 +52,7 @@ char *str_addChar (char *str, char c)
 unsigned int nbr_spaces(char *s)
 {
 	int i, cmpt = 0;
-	
+
 	for (i = 0; s[i + 1] != '\0'; i++)
 	{
 		if (s[i]  == ' ' && s[i + 1] != ' ')
@@ -71,7 +72,7 @@ char **strtow(char *str)
 {
 	int i;
 	int spaces = nbr_spaces(str);
-	char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+	char **tokens = NULL;
 	char *token;
 	int checkingSpace = 0;
 	int word = 0;
@@ -81,7 +82,6 @@ char **strtow(char *str)
 		printf("Failed");
 		return (0);
 	}
-		
 
 	printf("looping");
 	for (i = 0; str[i] != '\0'; i++)
@@ -92,7 +92,7 @@ char **strtow(char *str)
 			{
 				word++;
 				checkingSpace = 1;
-			} 
+			}
 		}
 		else
 		{

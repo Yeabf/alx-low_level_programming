e276ad2891ab7b1c9911c899aa4811f0d5a0829c
Yeabf <yeabsiraf2@gmail.com>
2022-07-06 06:59:35 -0700

    c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *print_name - prints a name
+ * @name: name of a person
+ * @f: pointer to a function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+f(name);
+}

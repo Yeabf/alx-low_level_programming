58e172e0c72d405b8ea53bb92d849d51cec140ca
Yeabf <yeabsiraf2@gmail.com>
2022-07-06 09:29:42 -0700

    pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,10 +21,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
-		i++;
+	if (strcmp(s, ops[i].op) == 0)
+	return (ops[i].f);
+	i++;
 	}
-	
+
 	return (0);
 }
